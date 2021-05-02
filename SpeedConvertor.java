fe9d6331864f993f00bf01e19a00bade931958bb
KhushbooAhuja <72789106+KhushbooAhuja@users.noreply.github.com>
2021-05-02 19:43:22 +0530

    Add files via upload

---
+++ SpeedConvertor.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package udemyassignmnet;
+
+
+/**
+ *
+ * @author khusboo
+ */
+public class SpeedConvertor {
+     public static long toMilesPerHour(double kilometersPerHour){
+        if(kilometersPerHour<0){
+            return -1;
+        }
+        else{
+            return Math.round(kilometersPerHour / 1.609);
+        }
+    }
+    public static void printConversion(double kilometersPerHour){
+        if(kilometersPerHour<0){
+            System.out.println("Invalid Input");
+        }
+        else{
+            long milesPerHour = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour + "km/h =" + milesPerHour + "mi/h" );
+        }
+    }
+    public static void main(String args[]){
+        SpeedConvertor.printConversion(10.5);
+    }
+}
+
