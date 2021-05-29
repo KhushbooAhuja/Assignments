6f0d047a3721d97f93a189fdce8fda5ec3231fe7
KhushbooAhuja <72789106+KhushbooAhuja@users.noreply.github.com>
2021-05-29 10:28:24 +0530

    Add files via upload

---
+++ Ceiling.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package udemyassignmnet.Composition;
+
+/**
+ *
+ * @author khusboo
+ */
+public class Ceiling {
+    private int height;
+    private int paintedColor;
+ 
+    public Ceiling(int height, int paintedColor) {
+        this.height = height;
+        this.paintedColor = paintedColor;
+    }
+ 
+    public int getHeight() {
+        return height;
+    }
+ 
+    public int getPaintedColor() {
+        return paintedColor;
+    }
+}
+ 
--- Wall.java
+++ Wall.java
@@ -3,56 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package udemyassignmnet;
+package udemyassignmnet.Composition;
 
 /**
  *
  * @author khusboo
  */
 public class Wall {
-    private double width;
-    private double height;
-    
-    public Wall(double width, double height) {
-        this.width = width < 0 ? 0 : width;
-        this.height = height < 0 ? 0 : height;
+    private String direction;
+ 
+    public Wall(String direction) {
+        this.direction = direction;
     }
-    
-    public Wall() {
-        this(0, 0);
+ 
+    public String getDirection() {
+        return direction;
     }
-    
-    public double getWidth() {
-        return width;
-    }
-    
-    public double getHeight() {
-        return height;
-    }
-    
-    public void setWidth(double width) {
-        this.width = width < 0 ? 0 : width;
-    }
-    
-    public void setHeight(double height) {
-        this.height = height < 0 ? 0 : height;
-    }
-    
-    public double getArea() {
-        return width * height;
-    }
-    public static void main(String args[]){
-        Wall object = new Wall();
-        object.setHeight(2.5);
-        System.out.println(object.getHeight());
-        
-        Wall object1 = new Wall();
-        object1.setWidth(4.5);
-        System.out.println(object.getWidth());
-        
-        Wall object2 = new Wall();
-       // object2.setHeight(2.5);
-        System.out.println(object2.getArea());
-        
-        
-    }}
+}
